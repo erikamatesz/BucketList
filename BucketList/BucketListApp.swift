1d76da9734c72aba411f68c0bce3930fecb6a9fe
Erika Bueno <erikab@ciandt.com>
2022-12-11 16:28:19 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Erika C. Matesz Bueno on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
